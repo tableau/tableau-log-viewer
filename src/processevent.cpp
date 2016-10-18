654a4b35eefa7474bb88f73fc2f09566bd35ffe2
Terence Soh <terencesoh@users.noreply.github.com>
2016-10-18 10:57:10 -0700

    Fix plain text log messages with double quotes (#9)
    Previous fix to display plain text message correctly only address the
    "\" cases, but not messages with double quotes.
    The cleaner and faster fix is to just set the message string as a value
    of QJsonObject directly.
    
    Also fix a compilation warning in options.cpp.

--- src/processevent.cpp
+++ src/processevent.cpp
@@ -15,25 +15,26 @@ namespace ProcessEvent
         QStringList m_SkippedText = options.getSkippedText();
         QBitArray m_SkippedState = options.getSkippedState();
 
-        QJsonDocument jsonDoc;
         if (!message.startsWith("{"))
         {
-            auto str = QString("{\"idx\": %1, \"k\": \"\", \"v\": \"%2\"}")
-                    .arg(QString::number(index), message.replace("\\", "\\\\"));
-            jsonDoc = QJsonDocument::fromJson(str.toUtf8());
+            QJsonObject obj;
+            obj["idx"] = index;
+            obj["k"] = "";
+            obj["v"] = message;
+            return obj;
         }
         else
         {
             message.insert(1, QString("\"file\": \"%1\",").arg(fileName));
             message.insert(1, QString("\"idx\": %1,").arg(index));
-            jsonDoc = QJsonDocument::fromJson(message.toUtf8());
+            QJsonDocument jsonDoc = QJsonDocument::fromJson(message.toUtf8());
             if (jsonDoc.object().contains("k")
                     && m_SkippedText.contains(jsonDoc.object()["k"].toString())
                     && m_SkippedState[m_SkippedText.indexOf(jsonDoc.object()["k"].toString(), 0)])
             {
                 jsonDoc = QJsonDocument();
             }
+            return jsonDoc.object();
         }
-        return jsonDoc.object();
     }
 }
