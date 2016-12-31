6fd1bbdd0f5f932f6075e18489fef01db457d427
Luis Enciso <luiseeo@gmail.com>
2016-12-31 13:58:49 -0800

    Fix build break in Linux, change pow to std::pow

--- src/themeutils.cpp
+++ src/themeutils.cpp
@@ -16,9 +16,9 @@ double ThemeUtils::Luminance(QColor color)
     double r = color.redF();
     double g = color.greenF();
     double b = color.blueF();
-    r = (r <= 0.03928) ? r/12.93 : pow((r+0.055)/1.055, 2.4);
-    g = (g <= 0.03928) ? g/12.93 : pow((g+0.055)/1.055, 2.4);
-    b = (b <= 0.03928) ? b/12.93 : pow((b+0.055)/1.055, 2.4);
+    r = (r <= 0.03928) ? r/12.93 : std::pow((r+0.055)/1.055, 2.4);
+    g = (g <= 0.03928) ? g/12.93 : std::pow((g+0.055)/1.055, 2.4);
+    b = (b <= 0.03928) ? b/12.93 : std::pow((b+0.055)/1.055, 2.4);
     return 0.2126 * r + 0.7152 * g + 0.0722 * b;
 }
 
